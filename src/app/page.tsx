c0b285c5879d4bcf6472c2c0a2f0df389e7c7460
joooooooods <82227954+joooooooods@users.noreply.github.com>
2024-05-27 18:46:45 +0900

    Update page.tsx
    랜딩에서 스크린으로 가는 버튼 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       랜딩
+      <a href="http://localhost:3000/screen">
+        <button>Go to Screen</button>
+    </a>
     </main>
   );
 }
